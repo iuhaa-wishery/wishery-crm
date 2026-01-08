ec16e4ea9e37ebccbe8cf5171c771f302a9159e5
sandeepkoovery <sandeepkoovery@gmail.com>
2026-01-08 12:34:53 +0530

    updates

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/wishery-crm/public/build/',
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
