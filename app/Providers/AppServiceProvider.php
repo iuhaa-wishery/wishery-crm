2c3974811d81681ef0425ab17d20a1ae5aea6b95
sandeepkoovery <sandeepkoovery@gmail.com>
2026-02-03 12:16:30 +0530

    updated code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,9 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // if (str_starts_with(config('app.url'), 'https://')) {
-        //     URL::forceScheme('https');
-        // }
+        URL::forceScheme('https');
     }
 }
 
